0bbd911d20a680eb2d04e3845697c0f5efd9838c
Simon Swartzman <simon.swartzman@gmail.com>
2018-03-03 00:09:38 -0600

    Got tests passing

--- test/classes/OptFlowAnalyzer.js
+++ test/classes/OptFlowAnalyzer.js
@@ -16,26 +16,33 @@ describe('OptFlowAnalyzer', function() {
     	let mockVideo = { test: "value" };
       let analyzer = new OptFlowAnalyzer(mockVideo);
       analyzer.init();
-      chai.assert.equal(analyzer.point_count, 0);      
+      chai.assert.equal(analyzer.point_count, 1);      
   	});
   });
 
   describe('#parseNext()', function() {
-  	it('should return ', function() {
+  	it('should return shifted values', function() {
   		let mockVideo = { test: "value" };
   		let analyzer = new OptFlowAnalyzer(mockVideo);
   		analyzer.init();
   		let uint1DataArray = uint1String.split(",");
 			let image1Data = Uint8ClampedArray.from(uint1DataArray);
   		analyzer.parseNext(image1Data);
+  		let reducedInitial = analyzer.curr_xy.reduce(function(total, current) {
+  			return total + current;
+  		});
 			let uint2DataArray = uint2String.split(",");
 			let image2Data = Uint8ClampedArray.from(uint2DataArray);
   		analyzer.parseNext(image2Data);
-  		analyzer.parseNext(image1Data);
-  		let reduced = analyzer.curr_xy.reduce(function(total, current) {
+  		// analyzer.parseNext(image1Data);
+  		let reducedEnd = analyzer.curr_xy.reduce(function(total, current) {
   			return total + current;
   		});
-  		chai.assert.equal(reduced, "something else");
+  		chai.assert.notEqual(reducedInitial, reducedEnd);
+  		chai.assert.notEqual(
+  			analyzer.curr_xy.findIndex(x => x === 64), 
+  			analyzer.prev_xy.findIndex(x => x === 64)
+  		);
   	});
   });
 });
