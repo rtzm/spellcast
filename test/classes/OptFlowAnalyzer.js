aeac017a909d2f07a0ced20ef2696a789a927f28
Simon Swartzman <simon.swartzman@gmail.com>
2018-03-04 17:31:28 -0600

    Make ImageData mock for testing

--- test/classes/OptFlowAnalyzer.js
+++ test/classes/OptFlowAnalyzer.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import OptFlowAnalyzer from '../../lib/classes/OptFlowAnalyzer';
+import MockImageData from '../testData/MockImageData';
 import {uint1String, uint2String} from '../testData/uints';
 
 describe('OptFlowAnalyzer', function() {
@@ -21,17 +22,19 @@ describe('OptFlowAnalyzer', function() {
   });
 
   describe('#parseNext()', function() {
-  	it('should return shifted values', function() {
+  	xit('should return shifted values', function() {
   		let mockVideo = { test: "value" };
   		let analyzer = new OptFlowAnalyzer(mockVideo);
   		analyzer.init();
   		let uint1DataArray = uint1String.split(",");
+      console.log("length of uint1DataArray is " + uint1DataArray.length);
 			let image1Data = Uint8ClampedArray.from(uint1DataArray);
   		analyzer.parseNext(image1Data);
   		let reducedInitial = analyzer.curr_xy.reduce(function(total, current) {
   			return total + current;
   		});
 			let uint2DataArray = uint2String.split(",");
+      console.log("length of uint2DataArray is " + uint2DataArray.length);
 			let image2Data = Uint8ClampedArray.from(uint2DataArray);
   		analyzer.parseNext(image2Data);
   		// analyzer.parseNext(image1Data);
@@ -43,6 +46,27 @@ describe('OptFlowAnalyzer', function() {
   			analyzer.curr_xy.findIndex(x => x === 64), 
   			analyzer.prev_xy.findIndex(x => x === 64)
   		);
+    });
+
+    it('should return ___ for image shifted left', function() {
+      let mockVideo = { test: "value" };
+      let analyzer = new OptFlowAnalyzer(mockVideo);
+      analyzer.init();
+
+      // Parse first mock frame
+      let mockFrame1 = new MockImageData();
+      mockFrame1.setData();
+      analyzer.parseNext(mockFrame1);
+
+      // Parse second mock frame
+      let mockFrame2 = mockFrame1.makeShiftedImage("left");
+      console.log(mockFrame2.data);
+      analyzer.parseNext(mockFrame2);
+
+      chai.assert.equal(
+        analyzer.prev_xy, 
+        0
+      );
   	});
   });
 });
---
+++ test/testData/MockImageData.js
@@ -0,0 +1,91 @@
+export default function MockImageData(width = 8, height = 6) {
+	this.perPixel = 4;
+	this.width = width;
+	this.height = height;
+	this.length = this.width * this.height * this.perPixel;
+	this.data = new Uint8ClampedArray(this.length);
+};
+
+/**
+ * Set the the mockImageData.data property to random value or 
+ * to match data passed in.
+ * 
+ * @param {Uint8ClampedArray} data optional data to be set; will
+ *                                 set to random colors if empty
+ * @return {MockImageData} fluid return of this object
+ */
+MockImageData.prototype.setData = function(data = null) {
+	
+	// no data given, make random data
+	if (!data) {
+		data = this.data.map(this.randomColor);
+	}
+
+	if (!(data instanceof Uint8ClampedArray)) {
+		if ((data.hasOwnProperty('length')) &&
+			  (data.length === this.length)) {
+			data = Uint8ClampedArray.from(data);
+		} else {
+			throw "Data cannot be set";
+		}
+	}
+	this.data = data;
+	return this;
+};
+
+/**
+ * Random value for a 256-color subpixel RBGA value
+ * 
+ * @return {Int} Number between 0 and 255
+ */
+MockImageData.prototype.randomColor = function() {
+	return Math.floor(Math.random() * 256);
+}
+
+/**
+ * Makes another mock for an image shifted 1 pixel in 
+ * this direction
+ * 
+ * @param  {string} direction "left", "right", "up", 
+ *                            or "down"
+ * @return {MockImageData}    The new shifted image
+ */
+MockImageData.prototype.makeShiftedImage = function(direction = null) {
+	let newImageObject =  new MockImageData(this.width, this.height);
+	
+	let lengthOfPixel = this.perPixel;
+	let lengthOfRow = lengthOfPixel * this.width;
+
+	// Make array from existing data that can be manipulated
+	let newDataArray = this.data.toString().split(",");
+	newDataArray = newDataArray.map(x => parseInt(x, 10));
+
+	switch (direction) {
+		case "right":
+			for (let i = 0; i < lengthOfPixel; i++) {
+				let subpixel = newDataArray.shift();
+				newDataArray.push(subpixel);
+			}
+			break;
+		case "left":
+			for (let i = 0; i < lengthOfPixel; i++) {
+				let subpixel = newDataArray.pop();
+				newDataArray.unshift(subpixel);
+			}
+			break;
+		case "up":
+			for (let i = 0; i < lengthOfRow; i++) {
+				let subpixel = newDataArray.pop();
+				newDataArray.unshift(subpixel);
+			}
+			break;
+		case "down":
+			for (let i = 0; i < lengthOfRow; i++) {
+				let subpixel = newDataArray.shift();
+				newDataArray.push(subpixel);
+			}
+			break;
+	}
+	newImageObject.setData(newDataArray);
+	return newImageObject;
+}
