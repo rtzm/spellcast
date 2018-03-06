fbcc5f2b44c88039b27c29916f30c0e09c9abea2
Simon Swartzman <simon.swartzman@gmail.com>
2018-03-05 21:50:36 -0600

    Begin glyph and glyphwriter classes

---
+++ test/classes/GlyphWriter.js
@@ -0,0 +1,18 @@
+import chai from 'chai';
+import GlyphWriter from '../../lib/classes/GlyphWriter';
+
+describe('GlyphWriter', function() {
+	describe('constructor', function() {
+		it("instantiates with correct width and height", function() {
+			let glyphWidth = 128;
+			let glyphHeight = 96;
+			let mockGlyph = {
+				width: glyphWidth,
+				height: glyphHeight
+			};
+			let writer = new GlyphWriter(mockGlyph);
+
+			chai.assert(writer.currentPosition, [ 64, 48 ]);
+		});
+	});
+});
