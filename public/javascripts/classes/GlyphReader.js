cc86e54e22c14fc3638ec98f9d89e169b49685fd
Simon Swartzman <simon.swartzman@gmail.com>
2018-03-11 15:03:29 -0500

    Clean up some unused files, add todos

--- public/javascripts/classes/GlyphReader.js
+++ public/javascripts/classes/GlyphReader.js
@@ -1,7 +1,11 @@
 /**
  * Reads canvas element and converts it into a symbol if possible
- * 
+ *
+ * @todo this class currently isn't being used, since TesseractWorker
+ * does not like bing loaded in through a module like I'm doing with my
+ * other classes. Need to either clean that up or remove this file.
  * @param {canvas} outputCanvas
+ * @param {Tesseract} tesseract the Tesseract OCR service
  */
 export default function GlyphReader(outputCanvas, tesseract) {
 	this.outputCanvas = outputCanvas;
@@ -10,6 +14,7 @@ export default function GlyphReader(outputCanvas, tesseract) {
 
 /**
  * Reads the outputContext and converts to a symbol
+ * @todo currently not used, see todo above
  */
 GlyphReader.prototype.read = function() {
 	console.log(this.tesseract);
